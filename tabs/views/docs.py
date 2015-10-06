3a37bef41ba90d86f7145d928b52db78f13a7c2c
Karl Eklund <keklund@ad.unc.edu>
2015-10-06 13:58:38 -0400

    serve documentation files through documentation link

--- tabs/views/docs.py
+++ tabs/views/docs.py
@@ -1,10 +1,36 @@
-from os import listdir
-from os.path import dirname, join, normpath
-from flask import Blueprint, render_template
+from os import walk
+from os.path import dirname, exists, join, normpath, relpath
+from flask import abort, Blueprint, render_template, request
 
 mod = Blueprint('docs', __name__, url_prefix='/docs')
+doc_path = normpath(join(dirname(__file__), '../../docs'))
 
 @mod.route('/')
 def index():
-    doc_path = normpath(join(dirname(__file__), '../../docs'))
-    return render_template('docs/index.html', data=listdir(doc_path))
+    data = []
+    for root, dnames, filenames in walk(doc_path):
+        if isinstance(filenames, list):
+            for f in filenames:
+                if 'swp' not in f and '~' not in f:
+                    data.append(join(relpath(root, doc_path), f))
+    return render_template('docs/index.html', data=data) 
+
+@mod.route('/<doc>')
+@mod.route('/researcher/<doc>')
+@mod.route('/supervisor/<doc>')
+def serve_doc(doc=None):
+    url = request.url
+    if 'researcher' in url:
+        subdir = 'researcher'
+    elif 'supervisor' in url:
+        subdir = 'supervisor'
+    else:
+        subdir = ''
+
+    documentation = join(doc_path, subdir, doc)
+    if exists(documentation):
+        with open(documentation, 'r') as f:
+            data = f.read()
+    else:
+        abort(404)
+    return render_template('docs/docs.html', doc=doc, data=data)
